14bd95d1b92071df4080ec522f049608a39afbe2
leehajeong <ljh1012kr@LAPTOP-CPB9HIFH.localdomain>
2021-03-23 13:59:08 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://leehajeong:1234@boiler-plate.xvjai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+const {User} = require('./models/User')
+const config = require('./config/key')
+// const bodyParser = require('body-parser')
+
+// application/x-www-form-urlencoded
+// express 4.16.0 버전에서는 bodyParser가 express generator에 내장되어 있어
+// 따로 설치하지 않아도 되어bodyParser 대신 express라고 씀
+app.use(express.urlencoded({extended:true}))
+// application/json
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://leehajeong:1234@boiler-plate.xvjai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요 여러분')
+})
+
+app.post('/register', (req, res) => {
+  // 회원가입 정보들을 client에서 가져오면 db에 넣어줌
+  const user = new User(req.body)
+  // mongodb 메서드
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -9,7 +9,7 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true, 
         // 공백지우기
-        unique,
+        unique: true,
         // only one
     },
     password: {
